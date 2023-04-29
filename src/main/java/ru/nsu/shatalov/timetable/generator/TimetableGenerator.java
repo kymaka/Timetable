70ef5ea6cf49a6e1a07baa01565eb27254982a06
kymaka <seafth345@gmail.com>
2023-04-30 00:02:58 +0700

    Added teachers.

--- src/main/java/ru/nsu/shatalov/timetable/generator/TimetableGenerator.java
+++ src/main/java/ru/nsu/shatalov/timetable/generator/TimetableGenerator.java
@@ -6,6 +6,7 @@ import ru.nsu.shatalov.timetable.model.constraint.Room;
 import ru.nsu.shatalov.timetable.model.constraint.Subject;
 
 import java.util.List;
+
 import ru.nsu.shatalov.timetable.model.constraint.Teacher;
 
 public class TimetableGenerator {
@@ -14,26 +15,31 @@ public class TimetableGenerator {
         int numberOfCourses = subjects.size();
         int numberOfRooms = rooms.size();
         int numberOfTimeSlots = 5;
-        
+
         int[] courseCapacities = {50, 20, 60, 40};
         int[] courseRoomTypes = {3, 1, 2, 3};
         int[] roomNumbers = new int[numberOfRooms];
         int[] roomCapacities = new int[numberOfRooms];
         int[] roomTypes = new int[numberOfRooms];
+        int[] teacherArray = new int[teachers.size()];
         for (int i = 0; i < numberOfRooms; i++) {
             roomNumbers[i] = i;
             roomCapacities[i] = rooms.get(i).getType().getCapacity();
             roomTypes[i] = rooms.get(i).getType().toInt();
+        }
+        for (int i = 0; i < teachers.size(); i++) {
+            teacherArray[i] = i;
         }
 
         // Model
         Model model = new Model("University Timetable");
 
         // Variables
-        IntVar[][] timetable = new IntVar[numberOfCourses][2]; //i - subject, [i][0] - room, [i][1] - timeslot
+        IntVar[][] timetable = new IntVar[numberOfCourses][3]; //i - subject, [i][0] - room, [i][1] - timeslot
         for (int i = 0; i < numberOfCourses; i++) {
             timetable[i][0] = model.intVar("Course_" + i + "_Room", roomNumbers);
             timetable[i][1] = model.intVar("Course_" + i + "_TimeSlot", 0, numberOfTimeSlots - 1);
+            timetable[i][2] = model.intVar("Course_" + subjects.get(i).getName() + "_Teacher", 0, teachers.size() - 1);
         }
 
         // Constraints
@@ -51,24 +57,41 @@ public class TimetableGenerator {
         for (int i = 0; i < numberOfCourses; i++) {
             model.arithm(timetable[i][0], "<", numberOfRooms).post();
             model.arithm(timetable[i][1], "<", numberOfTimeSlots).post();
+            model.arithm(timetable[i][2], "<", teachers.size()).post();
 
             // Checking for correct room type.
             IntVar roomTypeVar = model.intVar(roomTypes);
             model.element(roomTypeVar, roomTypes, timetable[i][0]).post();
-            model.arithm(roomTypeVar, "=",courseRoomTypes[i]).post();
+            model.arithm(roomTypeVar, "=", courseRoomTypes[i]).post();
 
             // Checking for right room capacity.
             IntVar roomCapacityVar = model.intVar(roomCapacities);
             model.element(roomCapacityVar, roomCapacities, timetable[i][0]).post();
             model.arithm(roomCapacityVar, ">=", courseCapacities[i]).post();
+
+            // Finding qualified teacher.
+            IntVar teacherVar = model.intVar(teacherArray);
+            model.element(teacherVar, teacherArray, timetable[i][2]).post();
+
+            for (int t = 0; t < teachers.size(); t++) {
+                for (int s = 0; s < teachers.get(t).getSubjects().size(); s++) {
+                    if (teachers.get(t).getSubjects().get(s).equals(subjects.get(i))) {
+                        model.arithm(teacherVar, "=", t).post();
+                    }
+                }
+            }
         }
 
         // Solve and display
         if (model.getSolver().solve()) {
             for (int i = 0; i < numberOfCourses; i++) {
-                System.out.println("Subject " + subjects.get(i).getName() + " -> Room: "
-                        + rooms.get(timetable[i][0].getValue()).getNumber() + ", Time Slot: "
-                        + timetable[i][1].getValue());
+                System.out.println
+                        (
+                                "Subject " + subjects.get(i).getName() + " -> Room: "
+                                        + rooms.get(timetable[i][0].getValue()).getNumber() + ", Time Slot: "
+                                        + timetable[i][1].getValue() + ",  Teacher: "
+                                        + teachers.get(timetable[i][2].getValue()).getName()
+                        );
             }
         } else {
             System.out.println("No solution found.");
--- src/main/java/ru/nsu/shatalov/timetable/model/constraint/Subject.java
+++ src/main/java/ru/nsu/shatalov/timetable/model/constraint/Subject.java
@@ -3,17 +3,25 @@ package ru.nsu.shatalov.timetable.model.constraint;
 import ru.nsu.shatalov.timetable.model.enums.RoomType;
 import ru.nsu.shatalov.timetable.model.enums.SubjectType;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Subject {
-    private String name;
 
-    private RoomType roomType;
+    private static final AtomicInteger idCount = new AtomicInteger(0);
 
-    private SubjectType subjectType;
+    private final int id;
+
+    private final String name;
+
+    private final RoomType roomType;
+
+    private final SubjectType subjectType;
 
     public Subject(String name, RoomType roomType, SubjectType subjectType) {
         this.name = name;
         this.roomType = roomType;
         this.subjectType = subjectType;
+        this.id = idCount.getAndIncrement();
     }
 
     public String getName() {
@@ -26,5 +34,9 @@ public class Subject {
 
     public SubjectType getSubjectType() {
         return this.subjectType;
+    }
+
+    public int getId() {
+        return id;
     }
 }
